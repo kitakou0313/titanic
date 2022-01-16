57f036f584b5491e27766da1f4c0681d66cb8b6c
kitakou0313 <kitakou0313@gmail.com>
2022-01-16 02:37:39 +0000

    Define model to predict with lgb

--- kaggle_book/ch7/models.py
+++ kaggle_book/ch7/models.py
@@ -1,7 +1,37 @@
 from tkinter.messagebox import NO
 import numpy as np
 import pandas as pd
+import lightgbm as lgb
 
+class ModleGBDT(object):
+    """
+    lightGBMによる推論
+    """
+    def init(self):
+        """
+        コンストラクタ
+        """
+        self.model = None
+    def fit(self, tr_x, tr_y, va_x, va_y):
+        """
+        学習
+        """
+        params = {'objective': 'binary', 'seed': 71, 'verbose': 0, 'metrics': 'binary_logloss'}
+        num_round = 100
+
+        dtrain = lgb.Dataset(tr_x, tr_y)
+        dvalid = lgb.Dataset(va_x, va_y)
+        
+        self.model = lgb.train(
+            params, dtrain, num_boost_round=num_round,valid_names=["train", "valid"], valid_sets=[dtrain, dvalid]
+        )
+
+    def predict(self, x):
+        """
+        推論用メソッド
+        """
+        return self.model.predict(x)
+        
 
 
 # 線形モデルの定義
